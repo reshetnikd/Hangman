ceddcf99fc411dc7489934c1f29b0426da01b80f
reshetnikd <dmitry.reshetnik@icloud.com>
2020-03-21 20:19:33 +0200

    Add basic layout of UI

--- Hangman/ViewController.swift
+++ Hangman/ViewController.swift
@@ -9,6 +9,96 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var scoreLabel: UILabel!
+    var levelLabel: UILabel!
+    var wordLabel: UILabel!
+    var alphabetButtons = [UIButton]()
+    
+    var allWords: [String] = []
+    var usedWords: [String] = []
+    
+    var alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+    var answers = 0
+    var score = 0 {
+        didSet {
+            scoreLabel.text = "Score: \(score)"
+        }
+    }
+    var level = 1 {
+        didSet {
+            levelLabel.text = "Level: \(level)"
+        }
+    }
+    
+    override func loadView() {
+        view = UIView()
+        view.backgroundColor = .white
+        
+        scoreLabel = UILabel()
+        scoreLabel.translatesAutoresizingMaskIntoConstraints = false
+        scoreLabel.textAlignment = .right
+        scoreLabel.text = "Score: 0"
+        view.addSubview(scoreLabel)
+        
+        levelLabel = UILabel()
+        levelLabel.translatesAutoresizingMaskIntoConstraints = false
+        levelLabel.textAlignment = .left
+        levelLabel.text = "Level: 1"
+        view.addSubview(levelLabel)
+        
+        wordLabel = UILabel()
+        wordLabel.setContentHuggingPriority(UILayoutPriority(1), for: .vertical)
+        wordLabel.translatesAutoresizingMaskIntoConstraints = false
+        wordLabel.font = UIFont.systemFont(ofSize: 72)
+        wordLabel.textAlignment = .center
+        wordLabel.text = "_ _ _ _ _ _ _ _"
+        wordLabel.numberOfLines = 1
+        view.addSubview(wordLabel)
+        
+        let buttonsView = UIView()
+        buttonsView.translatesAutoresizingMaskIntoConstraints = false
+        buttonsView.layer.borderWidth = 1
+        buttonsView.layer.cornerRadius = 8
+        buttonsView.layer.borderColor = UIColor.lightGray.cgColor
+        view.addSubview(buttonsView)
+        
+        NSLayoutConstraint.activate([
+            scoreLabel.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
+            scoreLabel.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
+            
+            levelLabel.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
+            levelLabel.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
+            
+            wordLabel.topAnchor.constraint(equalTo: scoreLabel.bottomAnchor, constant: 40),
+            wordLabel.widthAnchor.constraint(equalTo: view.layoutMarginsGuide.widthAnchor, multiplier: 0.8, constant: -100),
+            wordLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            buttonsView.widthAnchor.constraint(equalToConstant: 476),
+            buttonsView.heightAnchor.constraint(equalToConstant: 80),
+            buttonsView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            buttonsView.topAnchor.constraint(equalTo: wordLabel.bottomAnchor, constant: 20),
+            buttonsView.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor, constant: -20)
+        ])
+        
+        let width = 36
+        let height = 36
+        
+        for row in 0..<2 {
+            for col in 0..<13 {
+                let letterButton = UIButton(type: .system)
+                letterButton.titleLabel?.font = UIFont.systemFont(ofSize: 36)
+                
+                letterButton.setTitle(alphabet[row != 1 ? col : col + 12], for: .normal)
+                
+                let frame = CGRect(x: col * width, y: row * height, width: width, height: height)
+                letterButton.frame = frame
+                
+                buttonsView.addSubview(letterButton)
+                
+                alphabetButtons.append(letterButton)
+            }
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()

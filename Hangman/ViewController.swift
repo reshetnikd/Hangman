c81957047e299180b02cf9790ff3bd70e57f2e1c
reshetnikd <dmitry.reshetnik@icloud.com>
2020-03-21 23:21:16 +0200

    Implement game logic

--- Hangman/ViewController.swift
+++ Hangman/ViewController.swift
@@ -14,10 +14,10 @@ class ViewController: UIViewController {
     var wordLabel: UILabel!
     var alphabetButtons = [UIButton]()
     
-    var allWords: [String] = []
-    var usedWords: [String] = []
+    var allWords = [String]()
     
     var alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+    var word = ""
     var answers = 0
     var score = 0 {
         didSet {
@@ -51,7 +51,6 @@ class ViewController: UIViewController {
         wordLabel.translatesAutoresizingMaskIntoConstraints = false
         wordLabel.font = UIFont.systemFont(ofSize: 72)
         wordLabel.textAlignment = .center
-        wordLabel.text = "_ _ _ _ _ _ _ _"
         wordLabel.numberOfLines = 1
         view.addSubview(wordLabel)
         
@@ -70,7 +69,7 @@ class ViewController: UIViewController {
             levelLabel.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
             
             wordLabel.topAnchor.constraint(equalTo: scoreLabel.bottomAnchor, constant: 40),
-            wordLabel.widthAnchor.constraint(equalTo: view.layoutMarginsGuide.widthAnchor, multiplier: 0.8, constant: -100),
+            wordLabel.widthAnchor.constraint(equalTo: view.layoutMarginsGuide.widthAnchor, multiplier: 0.8, constant: -20),
             wordLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             
             buttonsView.widthAnchor.constraint(equalToConstant: 476),
@@ -80,22 +79,31 @@ class ViewController: UIViewController {
             buttonsView.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor, constant: -20)
         ])
         
+        // set some values for the width and height of each button
         let width = 36
         let height = 36
         
+        // create 26 buttons as a 2x13 grid
         for row in 0..<2 {
             for col in 0..<13 {
+                // create a new button and give it a big font size
                 let letterButton = UIButton(type: .system)
                 letterButton.titleLabel?.font = UIFont.systemFont(ofSize: 36)
                 
-                letterButton.setTitle(alphabet[row != 1 ? col : col + 12], for: .normal)
+                // give the button leter text from alphabet
+                letterButton.setTitle(alphabet[row != 1 ? col : col + 13], for: .normal)
                 
+                // calculate the frame of this button using its column and row
                 let frame = CGRect(x: col * width, y: row * height, width: width, height: height)
                 letterButton.frame = frame
                 
+                // add it to the buttons view
                 buttonsView.addSubview(letterButton)
                 
+                // and also to our alphabetButtons array
                 alphabetButtons.append(letterButton)
+                
+                letterButton.addTarget(self, action: #selector(letterTapped), for: .touchUpInside)
             }
         }
     }
@@ -103,6 +111,53 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        loadLevel()
+    }
+    
+    @objc func letterTapped(_ sender: UIButton) {
+        for (index, letter) in word.enumerated() {
+            if letter.uppercased() == sender.titleLabel?.text {
+                wordLabel.text = replace(inString: wordLabel.text!, at: index, with: Character(sender.titleLabel!.text!))
+                score += 1
+            }
+        }
+        answers += 1
+        
+        if answers == 7 {
+            loadLevel()
+        }
+        
+        if (wordLabel.text?.contains("_"))! {
+            return
+        } else {
+            level += 1
+            loadLevel()
+        }
+    }
+    
+    func loadLevel() {
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                allWords = startWords.components(separatedBy: "\n")
+                word = allWords.randomElement()!
+            }
+        }
+
+        var labelText = ""
+
+        for _ in word {
+            labelText.append("_")
+        }
+        
+        wordLabel.text = labelText
+        answers = 0
+    }
+    
+    func replace(inString: String, at index: Int, with newChar: Character) -> String {
+        var chars = Array(inString)     // gets an array of characters
+        chars[index] = newChar
+        let modifiedString = String(chars)
+        return modifiedString
     }
 
 
